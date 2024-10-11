245288cda8e0de538c0f6bac8e8db93a7e9f5d59
Duda <duduzinhacb@gmail.com>
2024-10-11 11:02:13 -0300

    Commit feito pelo Lindo do Professor :)

---
+++ FlappyBirdCerto/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿namespace FlappyBirdCerto;
+
+public partial class MainPage : ContentPage
+{
+	const int gravidade=3;
+	const int tempoEntreFrames=25;
+	bool estaMorto=true;
+	double larguraJanela=0;
+	double alturaJanela=0;
+	int velocidade=20;
+
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+		
+	}
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+		larguraJanela=width;
+    }
+	void GerenciaCanos ()
+	{
+		canocima.TranslationX -= velocidade;
+		canobaixo.TranslationX -= velocidade;
+		if(canobaixo.TranslationX<-larguraJanela)
+		{
+			canobaixo.TranslationX=0;
+			canocima.TranslationX=0;
+		}
+	}
+   
+    void AplicaGravidade ()
+	{
+		bird.TranslationY+= gravidade;
+	}
+	async Task Desenhar ()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			await Task.Delay(tempoEntreFrames);
+			GerenciaCanos();
+		}
+	}
+	void OnGameOverClicked (object sender, TappedEventArgs args)
+	{
+		FrameGameOver.IsVisible = false;
+		Inicializar();
+		Desenhar ();
+	}
+	void Inicializar()
+	{
+		estaMorto=false;
+		bird.TranslationY=0;
+	}
+}
+
+
---
+++ FlappyBirdCerto/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FlappyBirdCerto;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
